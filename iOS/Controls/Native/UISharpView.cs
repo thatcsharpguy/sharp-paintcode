efc28bc3f730ccfe10ceda0055299e5dc6bc8a48
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-03 00:47:14 -0500

    Add ctors to the native UIView

--- iOS/Controls/Native/UISharpView.cs
+++ iOS/Controls/Native/UISharpView.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using CoreGraphics;
 using UIKit;
 
 namespace SharpPaintCode.iOS.Controls.Native
 {
     public class UISharpView : UIView
     {
+        #region Constructores
+
+        public UISharpView(IntPtr p)
+            : base(p) { }
+
+        public UISharpView(CGRect rect) 
+            : base(rect) { }
+
+        public UISharpView() { }
+
+        #endregion
     }
 }
