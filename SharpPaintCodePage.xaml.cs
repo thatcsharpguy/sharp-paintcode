1f3db1136b6ee6f33a39a46c7e31b0fc656d7080
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-03 07:34:30 -0500

    Sample page

--- SharpPaintCodePage.xaml.cs
+++ SharpPaintCodePage.xaml.cs
@@ -8,5 +8,33 @@ namespace SharpPaintCode
         {
             InitializeComponent();
         }
+
+        int _currentColor = 0;
+        Color[] Colors = new Color[]
+        {
+            Color.Red, 
+            Color.Olive, 
+            Color.Green, 
+            Color.DodgerBlue, 
+            Color.DeepPink, 
+            Color.DarkOrange
+        };
+
+        void NextColorsButtonClicked(object sender, System.EventArgs e)
+        {
+            _currentColor++;
+            SetColor();
+        }
+
+        protected override void OnAppearing()
+        {
+            SetColor();
+        }
+
+        void SetColor()
+        {
+            Sharp1.FillColor = Colors[_currentColor % Colors.Length];
+            Sharp2.FillColor = Colors[(_currentColor+1) % Colors.Length];
+        }
     }
 }
