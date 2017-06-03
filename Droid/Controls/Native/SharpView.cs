f054fe0077b2dffc0bb42d604f8eec83c20b2a18
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-03 07:17:28 -0500

    Added actors to Native Droid view

--- Droid/Controls/Native/SharpView.cs
+++ Droid/Controls/Native/SharpView.cs
@@ -1,9 +1,22 @@
 ﻿using System;
-using Xamarin.Forms;
+using Android.Content;
+using Android.Util;
+using Android.Views;
 
 namespace SharpPaintCode.Droid.Controls.Native
 {
     public class SharpView : View
     {
+        #region Constructores
+
+        public SharpView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public SharpView(Context context) : base(context)
+        {
+        }
+
+        #endregion
     }
 }
