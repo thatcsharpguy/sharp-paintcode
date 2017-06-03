7d49a71628ea3a6ced7a2ad4c1b3413c26d20b4b
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-03 00:53:34 -0500

    Add SharpView renderer in iOS

---
+++ iOS/Controls/SharpViewRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SharpPaintCode.Controls;
+using SharpPaintCode.iOS.Controls.Native;
+using Xamarin.Forms.Platform.iOS;
+
+namespace SharpPaintCode.iOS.Controls
+{
+    public class SharpViewRenderer : ViewRenderer<SharpView, UISharpView>
+    {
+    }
+}
