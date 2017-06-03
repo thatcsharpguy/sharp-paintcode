cf5eef98a2a7b0168a9ecce55d3cdc7e39092e6e
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-03 07:20:36 -0500

    Add droid customrenderer

---
+++ Droid/Controls/SharpViewRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SharpPaintCode.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using FormsSharpView = SharpPaintCode.Controls.SharpView;
+using NativeSharpView = SharpPaintCode.Droid.Controls.Native.SharpView;
+
+[assembly: ExportRenderer(typeof(FormsSharpView), typeof(SharpViewRenderer))]
+namespace SharpPaintCode.Droid.Controls
+{
+    public class SharpViewRenderer : ViewRenderer<FormsSharpView, NativeSharpView>
+    {
+
+    }
+}
